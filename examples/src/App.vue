ce9e7bd07c2cd432385431b8d0a8b08f74da85c2
JB <jb@visualjerk.de>
2020-07-21 19:53:01 +0200

    docs: add examples

---
+++ examples/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Clickable @click="handleClick">Clickable</Clickable>
+    <Clickable @click="handleClick" disabled>Clickable disabled</Clickable>
+    <Clickable @click="handleClick" disabled focusable
+      >Clickable focusable</Clickable
+    >
+    <Clickable @click="handleClick" as="button">Clickable button</Clickable>
+    <Tabbable @click="handleClick">Tabbable</Tabbable>
+    <Tabbable @click="handleClick" disabled>Tabbable disabled</Tabbable>
+    <Tabbable @click="handleClick" disabled focusable
+      >Tabbable focusable</Tabbable
+    >
+    <Tabbable @click="handleClick" as="button">Tabbable button</Tabbable>
+  </div>
+</template>
+
+<script>
+import { Clickable } from 'ari-vue/src/clickable'
+import { Tabbable } from 'ari-vue/src/tabbable'
+
+export default {
+  name: 'App',
+  components: {
+    Clickable,
+    Tabbable,
+  },
+  methods: {
+    handleClick() {
+      alert('clicked')
+    },
+  },
+}
+</script>
