5dee666a48b2d4c911e1e4a33b2f276fb467b3f8
visualjerk <jb@visualjerk.de>
2022-01-22 18:41:40 +0100

    docs: nicer example styles

--- examples/simple-modal/src/App.vue
+++ examples/simple-modal/src/App.vue
@@ -63,13 +63,20 @@ export default defineComponent({
 
 <style>
 :root {
-  --space-1: 0.25rem;
-  --space-2: 0.5rem;
+  --space-1: 0.4rem;
+  --space-2: 0.7rem;
   --space-3: 1rem;
   --space-4: 1.5rem;
   --space-5: 2rem;
   --space-6: 4rem;
   --space-7: 6rem;
+
+  --font-size-sm: 0.85rem;
+
+  --font-weight-md: 500;
+  --font-weight-lg: 600;
+
+  --letter-spacing-md: 0.03em;
 
   --radius-sm: var(--space-1);
   --radius-base: var(--space-2);
@@ -88,14 +95,16 @@ export default defineComponent({
   --color-brand-inverse: hsl(var(--color-brand-h), var(--color-brand-s), 95%);
   --color-text: hsl(var(--color-brand-h), 12%, 14%);
   --color-border: hsl(var(--color-brand-h), 14%, 80%);
-  --color-surface: hsl(var(--color-brand-h), 10%, 97%);
-  --color-surface-alt: hsl(var(--color-brand-h), 10%, 99%);
+  --color-surface: hsl(var(--color-brand-h), 10%, 96%);
+  --color-surface-light: hsl(var(--color-brand-h), 10%, 99%);
+  --color-surface-dark: hsl(var(--color-brand-h), 10%, 92%);
+  --color-surface-dark-alt: hsl(var(--color-brand-h), 10%, 89%);
   --color-backdrop: hsla(var(--color-brand-h), 14%, 10%, 0.9);
 }
 
 body {
   padding: var(--space-4);
-  font-family: 'system-ui', sans-serif;
+  font-family: system-ui, sans-serif;
   color: var(--color-text);
 }
 
@@ -106,6 +115,16 @@ body {
   margin: 0;
 }
 
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: var(--font-weight-lg);
+  color: var(--color-brand);
+}
+
 .button {
   -webkit-appearance: none;
   -moz-appearance: none;
@@ -113,7 +132,15 @@ body {
   padding: var(--space-2) var(--space-3);
   border-radius: var(--radius-sm);
   font-size: 1rem;
+  text-transform: capitalize;
+  letter-spacing: var(--letter-spacing-md);
   cursor: pointer;
+  background-color: var(--color-surface-dark);
+  color: var(--color-text);
+}
+
+.button:hover {
+  background-color: var(--color-surface-dark-alt);
 }
 
 .button--primary {
@@ -136,6 +163,7 @@ body {
   justify-content: center;
   padding: var(--space-4);
   background-color: var(--color-backdrop);
+  backdrop-filter: blur(2px);
 }
 
 .modal {
@@ -146,12 +174,13 @@ body {
   padding: var(--space-4);
   background-color: var(--color-surface);
   border-radius: var(--radius-base);
+  border: 2px solid var(--color-surface-alt);
   box-shadow: var(--color-backdrop) 0 1rem 2rem;
 }
 
 .stack {
   display: grid;
-  gap: var(--space-3);
+  gap: var(--space-4);
 }
 
 .form-item {
@@ -165,12 +194,18 @@ body {
   gap: var(--space-2);
 }
 
+label {
+  padding-left: var(--space-1);
+  font-size: var(--font-size-sm);
+  font-weight: var(--font-weight-md);
+}
+
 input,
 textarea {
   padding: var(--space-2);
   border: 1px solid var(--color-border);
   border-radius: var(--radius-sm);
-  background-color: var(--color-surface-alt);
+  background-color: var(--color-surface-light);
 }
 
 textarea {
