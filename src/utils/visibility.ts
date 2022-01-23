1f4143c72161f044a080b78e6d5003135b475547
visualjerk <jb@visualjerk.de>
2022-01-23 21:42:02 +0100

    chore: cleanup visibility element

--- src/utils/visibility.ts
+++ src/utils/visibility.ts
@@ -9,24 +9,25 @@ import {
 } from 'vue'
 import { getElementFromRef } from './element'
 
-function setDisplay(el: RendererNode, value: unknown): void {
-  el.style.display = value ? el._vod : 'none'
-  el.hidden = value ? null : true
-}
-
 export function useVisibilityTransition(
   visibleRef: Ref<boolean>,
-  elementRef: Ref<HTMLElement> = null
+  elementRef: Ref<HTMLElement>
 ) {
   if (unref(visibleRef) == null) {
     return
   }
 
-  let vnode = null
+  let vnode
+  let initialDisplayValue
+
+  function setDisplay(el: RendererNode, value: unknown): void {
+    el.style.display = value ? initialDisplayValue : 'none'
+    el.hidden = value ? null : true
+  }
 
   watch(visibleRef, (visible) => {
     const { transition } = vnode
-    const el = elementRef ? getElementFromRef(elementRef) : vnode.el
+    const el = getElementFromRef(elementRef)
     if (!transition) {
       setDisplay(el, visible)
       return
@@ -49,8 +50,8 @@ export function useVisibilityTransition(
     const current = getCurrentInstance()
     vnode = current.vnode
     const { transition } = vnode
-    const el = elementRef ? getElementFromRef(elementRef) : vnode.el
-    el._vod = el.style.display === 'none' ? '' : el.style.display
+    const el = getElementFromRef(elementRef)
+    initialDisplayValue = el.style.display === 'none' ? '' : el.style.display
     if (transition && visible) {
       transition.beforeEnter(el)
       transition.enter(el)
